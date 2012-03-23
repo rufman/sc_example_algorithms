8c79be2d778bae83a3d7f59d83f564a456a53ae5
Stephane Rufer <stephane.rufer@uzh.ch>
2012-03-23 11:18:41 +0100

    finished clustering coefficient

--- ch/uzh/ifi/ddis/clustering_coefficient/ClusteringCoefficientVertex.java
+++ ch/uzh/ifi/ddis/clustering_coefficient/ClusteringCoefficientVertex.java
@@ -1,5 +1,11 @@
 package ch.uzh.ifi.ddis.clustering_coefficient;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.signalcollect.Edge;
 import com.signalcollect.javaapi.DataGraphVertex;
 
 /**
@@ -11,6 +17,45 @@ import com.signalcollect.javaapi.DataGraphVertex;
  */
 public class ClusteringCoefficientVertex
 		extends
-		DataGraphVertex<IdTypeParameter, StateTypeParameter, SignalTypeParameter> {
+		DataGraphVertex<Integer, ArrayList<Integer>, ArrayList<Integer>> {
+	
+	public ClusteringCoefficientVertex(Integer id, ArrayList<Integer> state){
+		super(id, state);
+	}
+	
+	public void initialize_state(ArrayList<Integer> state){
+		this.setState(state);
+	}
 
+	public ArrayList<Integer> collect(ArrayList<Integer> oldState, 
+										Iterable<ArrayList<Integer>> mostRecentSignals){
+		ArrayList<Integer> newState = (ArrayList<Integer>) (((ArrayList<Integer>) oldState).clone());
+		
+		/*ArrayList<Integer> list = new ArrayList<Integer>();
+		scala.collection.Iterator<scala.collection.Iterable<Edge>> it = this.getOutgoingEdges().iterator();
+		while (it.hasNext()){
+			 scala.collection.Iterator<Edge> eit = it.next().iterator();
+			 while (eit.hasNext()){
+				 Edge e = eit.next();
+				 list.add((Integer)e.id().targetId());
+			 }
+		}*/
+		
+		
+		for (ArrayList<Integer> neighbors : mostRecentSignals){
+			for (Integer n : neighbors){
+				newState.add(n);
+			}
+				/*Iterator<ClusteringCoefficientVertex> it = list.iterator();
+				while (it.hasNext()){
+					ClusteringCoefficientVertex v = it.next();
+					if (! newState.contains(v)){
+						newState.add(v);
+					}
+				}*/
+		}
+			
+		return newState;
+			
+	}
 }
